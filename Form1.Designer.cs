11a7016f18818dd4222af98b83127f1c49b1c1cf
ERICK GABRIEL MAJANO LOPEZ <egmajano@correo.url.edu.gt>
2023-02-19 19:04:15 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Estructuras1Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_leer = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label_test = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_leer
+            // 
+            this.button_leer.Location = new System.Drawing.Point(12, 279);
+            this.button_leer.Name = "button_leer";
+            this.button_leer.Size = new System.Drawing.Size(115, 36);
+            this.button_leer.TabIndex = 0;
+            this.button_leer.Text = "Cargar Datos";
+            this.button_leer.UseVisualStyleBackColor = true;
+            this.button_leer.Click += new System.EventHandler(this.button_leer_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label_test
+            // 
+            this.label_test.AutoSize = true;
+            this.label_test.Location = new System.Drawing.Point(133, 28);
+            this.label_test.Name = "label_test";
+            this.label_test.Size = new System.Drawing.Size(31, 13);
+            this.label_test.TabIndex = 1;
+            this.label_test.Text = "____";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Bar",
+            "Cafeteria",
+            "Hospital",
+            "Kindergarten",
+            "Vet",
+            "Panaderia",
+            "Mall",
+            "Salon",
+            "Farmacia",
+            "Gimnacio ",
+            "Tienda",
+            "Restaurante",
+            "Barberia",
+            "Clinica",
+            "Miselanea",
+            "Gasolinera"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 28);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(115, 244);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Servicios";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 325);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label_test);
+            this.Controls.Add(this.button_leer);
+            this.Name = "Form1";
+            this.Text = "Buscar Apartamentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_leer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label_test;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using QuickType;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+//usando el NuGet Newtwonsoft.json
+
+
+
+namespace Estructuras1Lab1
+{
+    public partial class Form1 : Form
+    {
+        public class inputsss
+        {
+            public Dictionary<String, bool>[] input1 { get; set; }
+            public String[] input2 { get; set; }
+        }
+        public class Apt
+        {
+            public bool Academy { get; set; }
+            public bool Gym { get; set; }
+            public bool Grocery { get; set; }
+            public bool Pharmacy { get; set; }
+            public bool Restaurant { get; set; }
+            public bool Beauty_salon { get; set; }
+            public bool Vet { get; set; }
+            public bool Hospital { get; set; }
+            public bool Clinic { get; set; }
+            public bool Mall { get; set; }
+            public bool Gas_Station { get; set; }
+            public bool Kindergarten { get; set; }
+            public bool Park { get; set; }
+            public bool Cofee_shop { get; set; }
+            public bool Barber_Shop { get; set; }
+        }
+        //public class Inputsss
+        //{
+        //    public Dictionary<string, bool> entrada { get; set; }
+        //    public Dictionary<string, bool> entrada2 { get; set; }
+        //}
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_leer_Click(object sender, EventArgs e)
+        {
+            //limitar los archivos que se pueden abrir
+            openFileDialog1.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //String jsonString = openFileDialog1.FileName;
+                String texto = File.ReadAllText(openFileDialog1.FileName);
+                int puesto = 0;
+                int ganador = 0;
+                int evaluado = 0;
+               // var welcome = Welcome.FromJson(jsonString);
+                label_test.Text = texto;
+                //guardar datos seleccionados
+                bool bar = checkedListBox1.GetItemChecked(0);
+                bool cafe = checkedListBox1.GetItemChecked(1);
+                bool hospital = checkedListBox1.GetItemChecked(2);
+                bool kinder = checkedListBox1.GetItemChecked(3);
+                bool vets = checkedListBox1.GetItemChecked(4);
+                bool panaderia = checkedListBox1.GetItemChecked(5);
+                bool mall = checkedListBox1.GetItemChecked(6);
+                bool salon = checkedListBox1.GetItemChecked(7);
+                bool farmacia = checkedListBox1.GetItemChecked(8);
+                bool gimanisum = checkedListBox1.GetItemChecked(9);
+                bool tienda = checkedListBox1.GetItemChecked(10);
+                bool restaurant = checkedListBox1.GetItemChecked(11);
+                bool barberia = checkedListBox1.GetItemChecked(12);
+                bool clinic = checkedListBox1.GetItemChecked(13);
+                bool miselanea = checkedListBox1.GetItemChecked(14);
+                bool gas = checkedListBox1.GetItemChecked(15);
+                inputsss inputs = JsonSerializer.Deserialize<inputsss>(texto);
+                foreach (Dictionary<String, bool> map in inputs.input1)
+                {
+                    label_test.Text = label_test.Text + ($"Apartamento: {puesto}") + "\n";
+                    foreach(KeyValuePair<String, bool> entry in map)
+                    {
+                        label_test.Text = label_test.Text + ($"Key {entry.Key} - vaulue {entry.Value}") + "\n";
+                        if ((entry.Key == "Bar" && bar == entry.Value)
+                            || (entry.Key == "CofeeShop" && entry.Value == cafe)
+                            || (entry.Key == "Hospital" && entry.Value == hospital)
+                            || (entry.Key == "Kindergarten" && entry.Value == kinder)
+                            || (entry.Key == "Vet" && entry.Value == vets)
+                            || (entry.Key == "Backery" && entry.Value == panaderia)
+                            || (entry.Key == "Mall" && entry.Value == mall)
+                            || (entry.Key == "Salon" && entry.Value == salon)
+                            || (entry.Key == "Pharmacy" && entry.Value == farmacia)
+                            || (entry.Key == "Gym" && entry.Value == gimanisum)
+                            || (entry.Key == "Store" && entry.Value == tienda)
+                            || (entry.Key == "Restaurant" && entry.Value == restaurant)
+                            || (entry.Key == "BarberShop" && entry.Value == barberia)
+                            || (entry.Key == "Clinic" && entry.Value == clinic)
+                            || (entry.Key == "GroceryStore" && entry.Value == miselanea)
+                            || (entry.Key == "GasStation" && entry.Value == gas))
+                        {
+                            evaluado++;
+                        }
+                    }
+                    puesto++;
+
+                }
+
+
+                //foreach (QuickType.Welcome w in welcome)
+               /* {
+                    texto = Convert.ToString(w.Bakery);
+                    texto= texto + "\n" + Convert.ToString(w.Vet);
+                    texto = texto + "\n" + Convert.ToString(w.Kindergarten);
+                    texto = texto + "\n" + Convert.ToString(w.CofeeShop);
+                    texto = texto + "\n" + Convert.ToString(w.Bakery);
+                    texto = texto + "\n" + Convert.ToString(w.Bar);
+                    texto = texto + "\n" + Convert.ToString(w.Hospital);
+                }*/
+            }
+        }
+
+        private void button_buscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
